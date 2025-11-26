dc6396fea803472db5677df0464f09c87af05c90
Fatima <fatima.arab@gmail.com>
2025-11-25 22:03:35 -0500

    Initial commit: resume builder client

---
+++ client/src/components/home/Banner.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+  
+      <div className="w-full py-2.5 font-medium text-sm text-purple-900 text-center  bg-gradient-to-r from-[#C084FC] to-[#E9D5FF]">
+            <p><span className="px-3 py-1 rounded-lg text-white bg-purple-600 mr-2">New</span>AI Feature Added</p>
+      </div> 
+ 
+  
+  )
+}
+
+export default Banner
