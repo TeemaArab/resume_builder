a22e9ba33f97582d2398ad589c43b1a531d74599
Fatima <fatima.arab@gmail.com>
2025-11-28 16:13:48 -0500

    fourth commit 'delete icon functionality for rsume done'

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -31,9 +31,25 @@ const Dashboard = () => {
 
    const uploadResume = async(event)=>{
     event.preventDefault()
-    setShowUploadResume(false)
-    navigate(`/app/builder/resume123`)
+    
    }
+
+// edit resume title function
+   const editTitle = async(event)=>{
+    event.preventDefault()
+    setEditResumeId('')
+   }
+
+ 
+   // delete resume function
+    const deleteResume = async(resumeId)=>{
+      // delete resume logic here
+      const confirm = window.confirm("Are you sure you want to delete this resume?")
+      if(confirm){
+        setAllResumes(prev => prev.filter(resume => resume._id !== resumeId))
+      }
+    }
+
   //to execute function
   useEffect(()=>{
     loadAllResumes()
@@ -58,11 +74,15 @@ const Dashboard = () => {
 
        <hr className='border-slate-300 my-6 sm:w-[305px]' />
 
+{/* ------------------------------------------------------------------------------------------------------------------------------------------------------------ */}
+{/*  list of available resume */}
        <div className='grid grid-cols-2 sm:flex flex-wrap gap-4'>
           {allResumes.map((resume,index)=>{
             const baseColor = colors[index % colors.length];
             return(
-              <button key={index} className= ' relative w-full sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg gap-2 border border group hover:shadow-lg transition-all duration-300 cursor-pointer'
+
+              // nvigate is used to go to special user's resume page
+              <button key={index} onClick ={()=>navigate(`/app/builder/${resume.id}`)} className= ' relative w-full sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg gap-2 border border group hover:shadow-lg transition-all duration-300 cursor-pointer'
                style={{background:`linear-gradient(135deg, ${baseColor}10, ${baseColor}40)`, borderColor: baseColor + '40'}}>
 
                 <FilePenLineIcon className='size-7 group-hover:scale-105 transition-all ' style={{color: baseColor}} />
@@ -70,16 +90,17 @@ const Dashboard = () => {
 
                 <p className='absolute bottom-1 text-[11px] text-slate-400 group-hover:text-slate-500 transition-all duration-300 px-2 text-center' style={{color: baseColor + '90'}}>Updated on {new Date(resume.updatedAt).toLocaleDateString()}</p>
                 
-                <div className='absolute top-1 right-1 group-hover:flex items-center hidden'>
-                 <TrashIcon className='size-7 p-1.5 hover:bg-white/50 rounded text-slate-700 transition-colors'/>
-                 <PencilIcon className='size-7 p-1.5 hover:bg-white/50 rounded text-slate-700 transition-colors'/>
+                <div onClick={e => e.stopPropagation()} className='absolute top-1 right-1 group-hover:flex items-center hidden'>
+                 <TrashIcon onClick={()=>deleteResume(resume._id)} className='size-7 p-1.5 hover:bg-white/50 rounded text-slate-700 transition-colors'/>
+                 <PencilIcon  onClick={()=>{setEditResumeId(resume._id); setTitle(resume.title)}} className='size-7 p-1.5 hover:bg-white/50 rounded text-slate-700 transition-colors'/>
                 </div>
               </button>
             )
           })}
        </div>
+{/* ------------------------------------------------------------------------------------------------------------------------------------------------------------ */}
 
-       {/* code for create and upload resume modals */}
+       {/* code for create and upload resume modals box */}
        {showCreateResume &&(
          <form  onSubmit={createResume} onClick={()=>setShowCreateResume(false)} className='fixed inset-0 bg-black/70  backdrop-blur bg-opacitiy-50 z-10 flex items-center justify-center'>
              <div onClick={e => e.stopPropagation()} className='relative bg-slate-50 border shadow-md rounded-lg w-full max-w-sm p-6'>
@@ -91,6 +112,8 @@ const Dashboard = () => {
          </form>
        )
        }
+{/* ---------------------------------------------------------------------------------------------------------- */}
+       {/*  this  section is for uploading resume */}
 
        {showUploadResume &&(
         <form  onSubmit={uploadResume} onClick={()=>setShowUploadResume(false)} className='fixed inset-0 bg-black/70  backdrop-blur bg-opacitiy-50 z-10 flex items-center justify-center'>
@@ -120,6 +143,19 @@ const Dashboard = () => {
                 </div>
               <button className='w-full py-2 bg-purple-600 text-white rounded hover:bg-purple-700 transition-colors' >Create Resume</button>
               <XIcon className='absolute top-4 right-4 text-slate-400 hover:text-slate-600 cursor-pointer transition-colors '  onClick={()=>{setShowUploadResume(false); setTitle('')}}/>
+             </div>
+         </form>
+       )
+       }
+     {/* ------------------------------------------------------------------------------------------------------------------------------------------------------------ */}
+      {/* this section is for edit icon of each available resume */}
+        {editResumeId &&(
+         <form  onSubmit={editTitle} onClick={()=>setEditResumeId('')} className='fixed inset-0 bg-black/70  backdrop-blur bg-opacitiy-50 z-10 flex items-center justify-center'>
+             <div onClick={e => e.stopPropagation()} className='relative bg-slate-50 border shadow-md rounded-lg w-full max-w-sm p-6'>
+              <h2 className='text-xl font-bold mb-4'>Edit Resume Title</h2>
+              <input  onChange={(e)=>setTitle(e.target.value )} value={title}type="text" placeholder='enter resume title' className='w-full px-4 py-2 mb-4 focus:border-purple-600 ring-purple-600 ' required/>
+              <button className='w-full py-2 bg-purple-600 text-white rounded hover:bg-purple-700 transition-colors' >Update</button>
+              <XIcon className='absolute top-4 right-4 text-slate-400 hover:text-slate-600 cursor-pointer transition-colors '  onClick={()=>{setEditResumeId(''); setTitle('')}}/>
              </div>
          </form>
        )
